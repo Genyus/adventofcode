80e389f9b90f083e3a9cc9e900d4e36f485045bb
Genyus <genyus@gmail.com>
2023-12-05 01:20:04 +0000

    feat: implement Part 2
    Brute-force nested loop to check for every "digit" at every position

--- 2023/day1/index.js
+++ 2023/day1/index.js
@@ -2,6 +2,41 @@ const fs = require("fs");
 const readline = require("readline");
 const path = require("path");
 
+const tokens = new Map([
+  ["0", 0],
+  ["1", 1],
+  ["2", 2],
+  ["3", 3],
+  ["4", 4],
+  ["5", 5],
+  ["6", 6],
+  ["7", 7],
+  ["8", 8],
+  ["9", 9],
+  ["zero", 0],
+  ["one", 1],
+  ["two", 2],
+  ["three", 3],
+  ["four", 4],
+  ["five", 5],
+  ["six", 6],
+  ["seven", 7],
+  ["eight", 8],
+  ["nine", 9],
+]);
+const getLineNumbers = (line) => {
+  const numbers = [];
+
+  for (let i = 0; i < line.length; i++) {
+    for (let substring of tokens.keys()) {
+      if (line.startsWith(substring, i)) {
+        numbers.push(tokens.get(substring));
+      }
+    }
+  }
+
+  return numbers;
+};
 const inputFile = path.join(__dirname, "input.txt");
 
 try {
@@ -13,10 +48,7 @@ try {
   let total = 0;
 
   interface.on("line", (line) => {
-    const numbers = line
-      .split("")
-      .filter((char) => !isNaN(char))
-      .map((char) => Number(char));
+    const numbers = getLineNumbers(line);
 
     if (numbers.length > 0) {
       total += numbers[0] * 10 + numbers[numbers.length - 1];
