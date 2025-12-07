709bf1c2bb73811369cce2d132960576957cd840
Genyus <genyus@gmail.com>
2025-12-06 23:21:55 -0400

    feat: add solution for Day 3, Part 2

--- src/2025/day3/index.ts
+++ src/2025/day3/index.ts
@@ -3,47 +3,99 @@ import { fileURLToPath } from "url";
 import { processInputFile } from "../utils/io.js";
 
 interface Context {
-  maxJoltage: number;
+  totalJoltage: number;
+  requiredBatteries: number;
 }
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const inputFile = path.join(__dirname, "input.txt");
-const context: Context = { maxJoltage: 0 };
 
 processInputFile(
   inputFile,
   (line, context) => {
-    context.maxJoltage += getBankJoltage(line.split("").map(Number));
+    context.totalJoltage += getBankJoltage(
+      line.split("").map(Number),
+      context.requiredBatteries,
+    );
   },
   (context) => {
-    console.log(`Max Joltage: ${context.maxJoltage}`);
+    displayCompletionMessage(context);
   },
-  context,
+  { totalJoltage: 0, requiredBatteries: 2 },
 );
-const getJolts = (left: number, right: number): number => {
-  return Number(`${left}${right}`);
+
+processInputFile(
+  inputFile,
+  (line, context) => {
+    context.totalJoltage += getBankJoltage(
+      line.split("").map(Number),
+      context.requiredBatteries,
+    );
+  },
+  (context) => {
+    displayCompletionMessage(context);
+  },
+  { totalJoltage: 0, requiredBatteries: 12 },
+);
+
+/**
+ * Gets the total joltage for the given array of batteries
+ * @param {number[]} arr - the array of batteries
+ * @returns {number} the joltage
+ */
+const getJolts = (arr: number[]): number => {
+  return Number(`${arr.join("")}`);
 };
-const getBankJoltage = (bank: number[]): number => {
-  let maxJolts = 0;
+/**
+ * Updates the active batteries by appending a new battery if it is valid.
+ * Preserves the array length by removing the lowest joltage battery
+ * @param {number} newBattery - the new battery to append
+ * @param {number[]} activeBatteries - the current active batteries
+ * @returns {number[]} the updated batteries
+ */
+const updateActiveBatteries = (
+  newBattery: number,
+  activeBatteries: number[],
+): number[] => {
+  const updatedBatteries = [...activeBatteries];
+  const index = updatedBatteries.findIndex(
+    (battery, index) => battery < updatedBatteries[index + 1]!,
+  );
+
+  if (index >= 0) {
+    updatedBatteries.splice(index, 1);
+    updatedBatteries.push(newBattery);
+  } else if (updatedBatteries[updatedBatteries.length - 1]! < newBattery) {
+    updatedBatteries.splice(updatedBatteries.length - 1, 1, newBattery);
+  }
+
+  return updatedBatteries;
+};
+/**
+ * Gets the total joltage for the active batteries in the given bank
+ * @param {number[]} bank - the bank of batteries
+ * @param {number} requiredBatteries - the number of required active batteries
+ * @returns {number} the total joltage
+ */
+const getBankJoltage = (bank: number[], requiredBatteries: number): number => {
+  let activeBatteries = Array.from(bank.slice(0, requiredBatteries));
+  let maxJolts = getJolts(activeBatteries);
+  let currentBatteries: number[] = [];
   let currentJolts = 0;
-  let left = 0;
-  let nextLeft = 0;
 
-  for (let right = 1; right < bank.length; right++) {
-    currentJolts = getJolts(bank[left]!, bank[right]!);
+  for (let next = requiredBatteries; next < bank.length; next++) {
+    currentBatteries = updateActiveBatteries(bank[next]!, activeBatteries);
+    currentJolts = getJolts(currentBatteries);
 
-    if (bank[right - 1]! > bank[left]!) {
-      nextLeft = right - 1;
+    if (currentJolts > maxJolts) {
+      activeBatteries = currentBatteries;
+      maxJolts = currentJolts;
     }
-
-    if (left !== nextLeft) {
-      left = nextLeft;
-      currentJolts = getJolts(bank[left]!, bank[right]!);
-    }
-
-    maxJolts = Math.max(maxJolts, currentJolts);
   }
 
   return maxJolts;
+};
+const displayCompletionMessage = (context: Context) => {
+  console.log(`Total Joltage: ${context.totalJoltage}`);
 };
